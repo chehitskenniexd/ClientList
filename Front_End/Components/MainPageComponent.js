38771e52d6a0e5bd9513318e762b084fd9ae3adc
Kenneth Moy <moykenneth91@gmail.com>
2018-01-19 02:42:53 -0500

    basic rendering of a possible homepage

--- Front_End/Components/MainPageComponent.js
+++ Front_End/Components/MainPageComponent.js
@@ -5,7 +5,11 @@ import { Link } from 'react-router';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
-import Button from 'material-ui/Button';
+import Card from 'material-ui/Card';
+import Textfield from 'material-ui/Textfield';
+import { withStyles } from 'material-ui/styles';
+import Checkbox from 'material-ui/Checkbox';
+import { FormGroup, FormControlLabel } from 'material-ui/Form';
 
 export default class MainPage extends React.Component {
     constructor(props) {
@@ -18,13 +22,58 @@ export default class MainPage extends React.Component {
                 <AppBar position="static" color="default">
                     <Toolbar>
                     <Typography type="title" color="inherit">
-                        Title
+                        Client List
                     </Typography>
                     </Toolbar>
                 </AppBar>
-                <Button raised className="button">
-                    Default
-                </Button>
+                <Textfield 
+                    className="first-name-input textfield"
+                    label="First Name"
+                    margin="normal"
+                />
+                <Textfield 
+                    className="last-name-input textfield"
+                    label="Last Name"
+                    margin="normal"
+                />
+                <Textfield 
+                    className="tp-ssn-input textfield"
+                    label="Taxpayer SSN"
+                    margin="normal"
+                />
+                <Textfield 
+                    className="sp-ssn-input textfield"
+                    label="Spouse SSN"
+                    margin="normal"
+                />
+                <FormGroup row>
+                    <FormControlLabel
+                        control={
+                        <Checkbox
+                            className="eic-checkbox muicheckbox"
+                        />
+                        }
+                        label="EIC with children"
+                    />
+                    <FormControlLabel
+                        control={
+                        <Checkbox
+                            className="miss-docs-checkbox muicheckbox"
+                        />
+                        }
+                        label="Missing Documents"
+                    />
+                </FormGroup>
+                <Textfield 
+                    className="telephone-input textfield"
+                    label="Telephone Number"
+                    margin="normal"
+                />
+                <Textfield 
+                    className="fee-input textfield"
+                    label="Fee"
+                    margin="normal"
+                />
                 {this.props.children}
             </div>
         );
