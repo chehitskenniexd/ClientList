648dff904082c1926e619b083f76a832f9179555
Kenneth Moy <moykenneth91@gmail.com>
2018-01-23 00:33:45 -0500

    added a basic state to the component and now tracks the state of the page

--- Front_End/Components/MainPageComponent.js
+++ Front_End/Components/MainPageComponent.js
@@ -14,8 +14,77 @@ import { FormGroup, FormControlLabel } from 'material-ui/Form';
 export default class MainPage extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            firstName: '',
+            lastName: '',
+            tpSSN: -1,
+            spSSN: -1,
+            bEIC: false,
+            bMissingDocs: false,
+            preparer: '',
+            phoneNum: '',
+            fee: -1,
+            comments: ''
+        }
+
+        this.onInputChangeFirstName = this.onInputChangeFirstName.bind(this);
+        this.onInputChangeLastName = this.onInputChangeLastName.bind(this);
+        this.onInputChangeTPSSN = this.onInputChangeTPSSN.bind(this);
+        this.onInputChangeSPSSN = this.onInputChangeSPSSN.bind(this);
+        this.onInputChangeBoolEIC = this.onInputChangeBoolEIC.bind(this);
+        this.onInputChangeBoolMissing = this.onInputChangeBoolMissing.bind(this);
+        this.onInputChangePreparer = this.onInputChangePreparer.bind(this);
+        this.onInputChangePhoneNum = this.onInputChangePhoneNum.bind(this);
+        this.onInputChangeFee = this.onInputChangeFee.bind(this);
+        this.onInputChangeComments = this.onInputChangeComments.bind(this);
+        this.onCreateClient = this.onCreateClient.bind(this);
+    }
+
+    onInputChangeFirstName(event) {
+        this.setState({ firstName: event.target.value });
+    }
+
+    onInputChangeLastName(event) {
+        this.setState({ lastName: event.target.value });
+    }
+
+    onInputChangeTPSSN(event) {
+        this.setState({ tpSSN: event.target.value });
+    }
+
+    onInputChangeSPSSN(event) {
+        this.setState({ spSSN: event.target.value });
+    }
+
+    onInputChangeBoolEIC(event) {
+        this.setState({ bEIC: event.target.value });
+    }
+
+    onInputChangeBoolMissing(event) {
+        this.setState({ bMissingDocs: event.target.value });
+    }
+
+    onInputChangePreparer(event) {
+        this.setState({ preparer: event.target.value });
+    }
+
+    onInputChangePhoneNum(event) {
+        this.setState({ phoneNum: event.target.value });
+    }
+
+    onInputChangeFee(event) {
+        this.setState({ fee: event.target.value });
+    }
+
+    onInputChangeComments(event) {
+        this.setState({ comments: event.target.value });
+    }
+
+    onCreateClient(event) {
+
     }
     
+
     render() {
         return (
             <div>
@@ -30,21 +99,25 @@ export default class MainPage extends React.Component {
                     className="first-name-input textfield"
                     label="First Name"
                     margin="normal"
+                    onChange={this.onInputChangeFirstName}
                 />
                 <Textfield 
                     className="last-name-input textfield"
                     label="Last Name"
                     margin="normal"
+                    onChange={this.onInputChangeLastName}
                 />
                 <Textfield 
                     className="tp-ssn-input textfield"
                     label="Taxpayer SSN"
                     margin="normal"
+                    onChange={this.onInputChangeTPSSN}
                 />
                 <Textfield 
                     className="sp-ssn-input textfield"
                     label="Spouse SSN"
                     margin="normal"
+                    onChange={this.onInputChangeSPSSN}
                 />
                 <FormGroup row>
                     <FormControlLabel
@@ -65,14 +138,28 @@ export default class MainPage extends React.Component {
                     />
                 </FormGroup>
                 <Textfield 
+                    className="preparer-input textfield"
+                    label="Preparer"
+                    margin="normal"
+                    onChange={this.onInputChangePreparer}
+                />
+                <Textfield 
                     className="telephone-input textfield"
                     label="Telephone Number"
                     margin="normal"
+                    onChange={this.onInputChangePhoneNum}
                 />
                 <Textfield 
                     className="fee-input textfield"
                     label="Fee"
                     margin="normal"
+                    onChange={this.onInputChangeFee}
+                />
+                <Textfield 
+                    className="comments-input textfield"
+                    label="Comments"
+                    margin="normal"
+                    onChange={this.onInputChangeComments}
                 />
                 {this.props.children}
             </div>
